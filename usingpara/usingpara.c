4232c0705ea25d734ee2f723a990d1acf97f6314
mybays <mybays@qq.com>
2013-03-10 15:14:10 +0800

    usingpara

---
+++ usingpara/usingpara.c
@@ -0,0 +1,34 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int count=1;
+module_param(count,int,1);
+MODULE_PARM_DESC(count, "How many times the helloworld prints.");
+
+
+
+
+
+static int hello_init(void)
+{
+	int i;
+	for(i=0;i<count;i++)
+	{
+		printk(KERN_ALERT "Hello, world\n");
+	}
+	return 0;
+}
+
+static void hello_exit(void)
+{
+	printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+
+
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
